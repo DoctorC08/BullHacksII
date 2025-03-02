1d43e09f1a60b80d8e60f1948a0eea4b585c58a9
Christopher M <christopher_mao@branson.org>
2025-03-01 19:50:35 -0800

    fix bugs

--- matchups.py
+++ matchups.py
@@ -25,7 +25,9 @@ def find_best_weather_condition(team_abrv1, team_abrv2):
     merged_data['score_diff_team2'] = merged_data['offensive_score_team2'] - merged_data['defensive_score_team2']
     
     # Calculate the net score for team1 (the difference we care about)
-    merged_data['net_score_team1'] = merged_data['score_diff_team1'] / merged_data['score_diff_team2']
+    # merged_data['net_score_team1'] = merged_data['score_diff_team1'] - merged_data['score_diff_team2']
+    merged_data['net_score_team1'] = merged_data['offensive_score_team1'] + merged_data['defensive_score_team2'] - merged_data['offensive_score_team2'] - merged_data['defensive_score_team1']
+
     
     # Find the weather condition with the best score for team1
     best_weather = merged_data.loc[merged_data['net_score_team1'].idxmax(), 'Weather Classified']
@@ -33,13 +35,32 @@ def find_best_weather_condition(team_abrv1, team_abrv2):
     # Return the best weather condition
     return best_weather, merged_data['net_score_team1'].max()
 
+def calculate_score(team_abrv):
+    # Get the data for each team
+    team1_data = df[df['Team'] == team_abrv]
+    
+    
+
+    # Calculate the score difference for each weather condition
+    team1_data['score_diff_team1'] = team1_data['offensive_score'] - team1_data['defensive_score']
+    
+    # Calculate the net score for team1 (the difference we care about)
+    
+    # Find the weather condition with the best score for team1
+    # print(merged_data)
+    # Return the best weather condition
+    return team1_data['score_diff_team1']
+
 if __name__ == '__main__':
-    home_team = "NYY"
-    for team in teams:
-        if team != home_team:
-            best_weather, score = find_best_weather_condition(home_team, team)
-            worst_weather, neg_score = find_best_weather_condition(team, home_team)
-            # print(f"The best weather condition for {home_team} to play against {team} is: {best_weather} and the predicted advantage is {score}")
-            print(f"best weather condition ({team}): {best_weather} predicted advantage is {score / 100}")
-            print(f"worst weather condition ({team}): {worst_weather} predicted disadvantage is -{neg_score / 100}")
+    # home_team = "NYY"
+    # for team in teams:
+    #     if team != home_team:
+    #         best_weather, score = find_best_weather_condition(home_team, team)
+    #         worst_weather, neg_score = find_best_weather_condition(team, home_team)
+    #         # print(f"The best weather condition for {home_team} to play against {team} is: {best_weather} and the predicted advantage is {score}")
+    #         print(f"best weather condition ({team}): {best_weather} predicted advantage is {score / 100}")
+    #         print(f"worst weather condition ({team}): {worst_weather} predicted disadvantage is -{neg_score / 100}")
+    # calculate_score("SFG")
+    print(calculate_score("OAK"))
+    
 
--- plots/plotting.py
+++ plots/plotting.py
@@ -1,20 +1,25 @@
 from plots import plot_histogram
 import pandas as pd
 
-basic_path = 'datasets/ARI_dataset.csv'
+basic_path = 'datasets/concated_full_dataset.csv'
 
 columns = [
-    # "Temperature (°C)", 
-    # "Humidity (%)", 
-    # "Relative Wind Direction (°)", 
-    "Precipitation (mm)", 
-    "Snow (mm)", 
-    # "Wind Speed (m/s)", 
+    "Temperature (°C)", 
+    "Humidity (%)", 
+    "Relative Wind Direction (°)", 
+    # "Precipitation (mm)", 
+    # "Snow (mm)", 
+    "Wind Speed (m/s)", 
     # "COCO"
 ]
 
+
+# pd.set_option('display.max_columns', None)
+# pd.set_option('display.max_rows', None)
+
 # Read the CSV file into a DataFrame
 df = pd.read_csv(basic_path)
+# print(df.columns)
 
 # Call the plot_histogram function, passing the DataFrame (df) and columns
 plot_histogram(df, columns)
